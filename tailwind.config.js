7a4bed3a820f2d58d75b9292e92652846add0a6a
Rishabh Malik <workwithmalik97@gmail.com>
2021-09-18 18:21:12 +0530

    change in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.ts',
-  ],
+  purge: ['./src/**/*.html', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
